9a27a2b0857653a79b584f9423908132951218cd
Akif T <atugcu360u@gmail.com>
2022-12-28 17:53:58 -0500

    test git on the branch

--- force-app/main/default/lwc/earthquakeFilters/earthquakeFilters.js
+++ force-app/main/default/lwc/earthquakeFilters/earthquakeFilters.js
@@ -1,5 +1,6 @@
-import { LightningElement } from "lwc";
+import { LightningElement, api } from "lwc";
 
 export default class EarthquakeFilters extends LightningElement {
   // test git
+  @api recordId;
 }
