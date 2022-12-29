b381b1b7af3c705d06aa9a4cd9801037c021dcf4
Akif T <atugcu360u@gmail.com>
2022-12-29 18:20:01 -0500

    push #1

--- force-app/main/default/lwc/earthquakeFilters/earthquakeFilters.js
+++ force-app/main/default/lwc/earthquakeFilters/earthquakeFilters.js
@@ -1,7 +1,3 @@
-import { LightningElement, api } from "lwc";
+import { LightningElement } from "lwc";
 
-export default class EarthquakeFilters extends LightningElement {
-  // test git
-  @api recordXXId;
-  objectType;
-}
+export default class EarthquakeFilters extends LightningElement {}
